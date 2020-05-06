fe5128ac5f3a47b2cbad02029e9daccd631069ba
dpasc <damianpascale@outlook.com>
2020-05-06 15:41:19 +1000

    Added NodaTime library to assist with Date type in Postgresql

--- WebApplication/Domain/Data/EntityFrameworkModels/Models/Woolworths.cs
+++ WebApplication/Domain/Data/EntityFrameworkModels/Models/Woolworths.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NodaTime;
 
 namespace Domain.Data.EntityFrameworkModels.Models
 {
@@ -14,28 +15,28 @@ namespace Domain.Data.EntityFrameworkModels.Models
         //Minor or Major 
         public string TypeOfReview { get; set; }
         //Submission date for FoodCo Own Brand Products
-        public DateTime? SubDateForFoodCoOwnBrandProducts { get; set; }
+        public LocalDate? SubDateForFoodCoOwnBrandProducts { get; set; }
 
         //"Notice of Probable Delisting" letter sent to impacted suppliers
-        public DateTime? NoticeOfProbableDelisting { get; set; }
+        public LocalDate? NoticeOfProbableDelisting { get; set; }
 
         //Supplier engagement:
         //Submissions opened for review & Supplier recommendations for deletions
 
-        public DateTime? SuppliersEngagement { get; set; }
+        public LocalDate? SuppliersEngagement { get; set; }
 
         //Final submission date for Branded Products
-        public DateTime? FinalSubmissionDateForBrandedProducts { get; set; }
+        public LocalDate? FinalSubmissionDateForBrandedProducts { get; set; }
 
         //Results to Suppliers of New & Deleted Lines
         //"Notice of Final Delisting" letter sent to impacted suppliers
-        public DateTime? InfoOfNewAndDeletedLines { get; set; }
+        public LocalDate? InfoOfNewAndDeletedLines { get; set; }
 
         //Suppliers to provide all WNAS, WAF, WPF to Buyer
-        public DateTime? ProvideAllWnasWafWpfToBuyers { get; set; }
+        public LocalDate? ProvideAllWnasWafWpfToBuyers { get; set; }
 
         //VISUAL PLANOGRAM DUE TO STORES
-        public DateTime VisualPlanogramDueToStores { get; set; }
+        public LocalDate VisualPlanogramDueToStores { get; set; }
 
     }
 
