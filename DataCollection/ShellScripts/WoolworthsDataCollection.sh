ec534ca0ebf6fd2d095f07d3fb4e8e8272fabbdf
dpasc <damianpascale@outlook.com>
2020-05-13 12:39:06 +1000

    New records are added to database

--- DataCollection/ShellScripts/WoolworthsDataCollection.sh
+++ DataCollection/ShellScripts/WoolworthsDataCollection.sh
@@ -2,3 +2,4 @@
 
 python ~/Projects/plkn/DataCollection/Scraper/ScraperWow.py
 python ~/Projects/plkn/DataCollection/DataCleaner/WoolworthsDataCleaner.py
+python ~/Projects/plkn/DataCollection/DataManager/WoolworthsDataManager.py
