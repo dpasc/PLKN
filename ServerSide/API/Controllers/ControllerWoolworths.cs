2a46deb98154dcefda6fcc8d50ca4cd575e614e5
dpasc <damianpascale@outlook.com>
2020-05-14 10:30:41 +1000

    Configured dependency injection

---
+++ ServerSide/API/Controllers/ControllerWoolworths.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Data.EntityFrameworkModels.Models;
+using Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Components.Routing;
+using Domain.Repositories.RepoInterfaces;
+
+namespace API.Controllers{
+
+    [Route("api/ControllerWoolworths")]
+    public class ControllerWoolworths: ControllerMain<Woolworths,RepositoryWoolworths>
+    {
+        public ControllerWoolworths(RepositoryWoolworths repository):base(repository)
+        {
+            
+        }
+
+    [HttpGet]
+        public override async Task<IEnumerable<IEntity>> Get()
+        {
+            return await _repository.GetAllRecords();
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
