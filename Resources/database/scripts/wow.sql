7ebaa57a9b98ee0cdf309f9e14069c4bee5fe9e8
dpasc <damianpascale@outlook.com>
2020-05-23 19:31:06 +1000

    Db scripts

--- Resources/database/scripts/wow.sql
+++ Resources/database/scripts/wow.sql
@@ -1,4 +1,3 @@
-
 CREATE DATABASE woolworths_db WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_AU.UTF-8' LC_CTYPE = 'en_AU.UTF-8';
 
 
@@ -21,43 +20,56 @@ SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
+--
+-- Name: __EFMigrationsHistory; Type: TABLE; Schema: public; Owner: plkn
+--
 
-
-
-
-CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+CREATE TABLE public."__EFMigrationsHistory" (
     migration_id character varying(150) NOT NULL,
-    product_version character varying(32) NOT NULL,
-    CONSTRAINT pk___ef_migrations_history PRIMARY KEY (migration_id)
+    product_version character varying(32) NOT NULL
 );
+
 
 ALTER TABLE public."__EFMigrationsHistory" OWNER TO plkn;
 
-CREATE TABLE category_dev_schedule (
-    id serial NOT NULL,
+--
+-- Name: category_dev_schedule; Type: TABLE; Schema: public; Owner: plkn
+--
+
+CREATE TABLE public.category_dev_schedule (
+    id integer NOT NULL,
     hob character varying(40) NOT NULL,
     mm character varying(40) NOT NULL,
     cm_buyer text NOT NULL,
     sub_cat_name text NOT NULL,
     review_type text NOT NULL,
-    sub_date_foodco_products date NULL,
-    notice_of_probable_delisting date NULL,
-    suppliers_engagement date NULL,
-    final_submission_for_branded date NULL,
-    info_of_new_and_deleted_lines date NULL,
-    provide_all_wnas_waf_wpf_to_buyer date NULL,
+    sub_date_foodco_products date,
+    notice_of_probable_delisting date,
+    suppliers_engagement date,
+    final_submission_for_branded date,
+    info_of_new_and_deleted_lines date,
+    provide_all_wnas_waf_wpf_to_buyer date,
     visual_planogram_due_to_stores date NOT NULL,
-    date_added date NOT NULL DEFAULT (current_date),
-    CONSTRAINT pk_woolworths PRIMARY KEY (id)
+    date_added date DEFAULT CURRENT_DATE NOT NULL
 );
 
 
 ALTER TABLE public.category_dev_schedule OWNER TO plkn;
 
+--
+-- Name: category_dev_schedule_id_seq; Type: SEQUENCE; Schema: public; Owner: plkn
+--
+
+CREATE SEQUENCE public.category_dev_schedule_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
 
-
-
+ALTER TABLE public.category_dev_schedule_id_seq OWNER TO plkn;
 
 --
 -- Name: category_dev_schedule_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: plkn
@@ -87,10 +99,3 @@ ALTER TABLE ONLY public."__EFMigrationsHistory"
 
 ALTER TABLE ONLY public.category_dev_schedule
     ADD CONSTRAINT pk_woolworths PRIMARY KEY (id);
-
-
-
-
-
-INSERT INTO "__EFMigrationsHistory" (migration_id, product_version)
-VALUES ('20200511040558_Added timestamp', '3.1.3');
